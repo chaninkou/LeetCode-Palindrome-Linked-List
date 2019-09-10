bd7e210b7c1bb046feec78d289e536e3d9fe3f74
chaninkou <michaelkoulove@yahoo.com>
2019-09-09 23:23:48 -0700

    Very good problem. Got better understanding of linked list. Palindrome.

---
+++ src/padlindromeLinked/CheckPadlindromeLinkedListFunction.java
@@ -0,0 +1,58 @@
+package padlindromeLinked;
+
+public class CheckPadlindromeLinkedListFunction {
+	public boolean isPalindrome(ListNode head) {
+        // Fast will go next 2 nodes
+        ListNode fast = head;
+        
+        // Slow will go next node
+        ListNode slow = head;
+        
+        // Finding the middle point of the linked List
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        // odd node, let right side half smaller in order for them to have equal length
+        if(fast != null){
+            slow = slow.next;
+        }
+        
+        // Reverse the slow linked list
+        slow = reverse(slow);
+        
+        // Fast will become the head of the original linkedlist now
+        fast = head;
+        
+        // Comparing the slow and fast linked list
+        while(slow != null){
+            // If last node is not the same as first node, false
+            if(slow.val != fast.val){
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+    
+        return true;
+	}
+
+	// Reverse the Linked List
+	private ListNode reverse(ListNode head) {
+		// Creating a new Linked List
+		// The last node is always pointing to null
+		ListNode previous = null;
+		
+		while(head != null){
+			ListNode nextTemp = head.next;
+			head.next = previous;
+			previous = head;
+			head = nextTemp;
+	        
+		}
+		
+		return previous;
+	}
+}
+
---
+++ src/padlindromeLinked/Main.java
@@ -0,0 +1,72 @@
+package padlindromeLinked;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] input = {1,2,2,1,4};
+		ListNode head = insertNode(input);
+		
+		// Delete node 4 and display it
+		displayLinkedList(removeNode(head, 4));
+		
+		CheckPadlindromeLinkedListFunction solution = new CheckPadlindromeLinkedListFunction();
+		
+		System.out.println("Solution: " + solution.isPalindrome(head));
+		
+	}
+	
+	public static ListNode insertNode(int[] input){
+		ListNode dummyRoot = new ListNode(0);
+		ListNode previous = dummyRoot;
+		
+		for(int item : input){
+			previous.next = new ListNode(item);
+			previous = previous.next;
+		}
+	
+		return dummyRoot.next;
+	}
+	
+	public static void displayLinkedList(ListNode head){
+		System.out.println("Input: ");
+		ListNode current = head;
+		
+		while(current != null){
+			System.out.print(current.val + " -> ");
+			
+			if(current.next == null){
+				System.out.print("null");
+			}
+			
+			current = current.next;
+		}
+		
+		System.out.println(" ");
+	}
+	
+	public static ListNode removeNode(ListNode head, int val){
+		// New Linked List
+		ListNode fakeHead = new ListNode(0);
+		
+		// Point the fakehead to the head
+		fakeHead.next = head;
+		
+		// Keeping track of current, starts from head
+		ListNode current = head;
+		
+		// Keep track of previous node
+		ListNode dummyNode = fakeHead;
+		
+		while(current != null){
+			if(current.val == val){
+				dummyNode.next = current.next;
+			} else {
+				dummyNode = dummyNode.next;
+			}
+			
+			current = current.next;
+		}
+		
+		// Since fakeHead was just a temporary head
+		return fakeHead.next;
+	}
+}
